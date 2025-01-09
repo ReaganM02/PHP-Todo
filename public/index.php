ff4ec6791484f246f6bf403a6742ad4a6dc61a0d
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-01-09 11:15:06 +0800

    Added public index

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php 
+
+use Core\Router;
 const BASE_PATH =  __DIR__ . '/../';
 require_once BASE_PATH . 'core/functions.php';
+
+spl_autoload_register(function($class) {
+  loadFile("{$class}.php");
+});
+
+$router = new Router();
+loadFile('routes.php',  ['router' => $router]);
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+$router->route($uri, $method);
