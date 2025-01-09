bb4083a348b0ee0687c27f5962b3bf9f3110b6dd
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-01-09 11:14:11 +0800

    Added reusable functions

--- core/functions.php
+++ core/functions.php
@@ -1,4 +1,20 @@
 <?php
-function loadFile($path) {
+function loadFile($path, $data = []) {
+  extract($data);
   require_once BASE_PATH . $path;
+}
+
+function prettyArray($array) {
+  echo '<pre>';
+    print_r($array);
+  echo '</pre>';
+  die();
+}
+
+function loadController($controllerPath) {
+  require_once BASE_PATH . 'controllers/' . $controllerPath;
+}
+
+function loadView($viewPath) {
+  require_once BASE_PATH . 'views/' . $viewPath;
 }
