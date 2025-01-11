d4bcffa84ea6e4eb8461db9933ce5f0debcd1ec0
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-01-11 12:16:45 +0800

    Added a new file called store for handling the product creation functionality

---
+++ controllers/products/store.php
@@ -0,0 +1,54 @@
+<?php
+
+use Core\Validator;
+use Model\Product_model;
+
+loadFile('model/product.model.php');
+
+
+$title = $_POST['product-title'];
+$price = $_POST['product-price'];
+$status = $_POST['product-status'];
+$quantity =$_POST['product-quantity'];
+$description = $_POST['product-description'];
+$stocks = $_POST['product-status'];
+
+$errors = [];
+
+if(!Validator::required($title)) {
+  $errors['title'] = 'Title is required';
+}
+
+if(!Validator::required($price)) {
+  $errors['price'] = 'Price is required.';
+}
+
+if(!Validator::required($quantity)) {
+  $errors['quantity'] = 'Quantity is required.';
+}
+
+if(!Validator::required($description)) {
+  $errors['description'] = 'Description is required.';
+}
+
+if(!(int) $_POST['product-quantity'] >= 1) {
+  $errors['quantity'] = 'Quantity must be a number greater than 1.';
+}
+
+$sanitizedData = [
+  'title' => $title, 
+  'description' => strip_tags($description), 
+  'quantity' => intval($quantity), 
+  'stocks' => strip_tags($stocks), 
+  'price' => floatval( $price)
+];
+
+if(!empty($errors)) {
+ loadView('products/create.view.php', ['errors' => $errors, 'valueData' => $sanitizedData]);
+} else {
+  $create =  Product_model::create($sanitizedData);
+  if($create) {
+    redirectURL('/');
+  }
+}
+
