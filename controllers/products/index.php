4cd814bf503505f37d8215f822c686587175f5aa
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-01-11 12:16:20 +0800

    Get list of product controller

--- controllers/products/index.php
+++ controllers/products/index.php
@@ -1,3 +1,7 @@
 <?php
-echo 'Hello world';
-loadView('products/index.view.php');
+
+use Model\Product_model;
+loadFile('model/product.model.php');
+
+$products = Product_model::getProducts();
+loadView('products/index.view.php', ['products' => $products]);
