071a1bec24254cb22863b7aa30cd7cec5bb26c01
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-01-11 12:15:27 +0800

    Added config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'database' => [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname'=> 'todo_products',
+    'charset' => 'utf8mb4'
+  ],
+  'user' => 'root',
+  'password' => '123456'
+];
