da4ec7946f19f6db44ed4ff60542f64555effed6
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-01-12 07:24:38 +0800

    Added stylesheets

--- views/products/index.view.php
+++ views/products/index.view.php
@@ -5,24 +5,31 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>List of Products</title>
   <?php echo loadFont(); ?>
+  <?php loadStylesheet('list-of-products.css') ?>
+  <?php loadStylesheet('main.css') ?>
 </head>
 <body>
-  <div class="with-overlay"></div>
   <main class="center-content list-of-product-content">
     <div class="list-of-products">
-       <div class="heading-and-action">
-        <div>
-          <h1>List of Products</h1>
-        </div>
-        <div>
-          <a href="/product/create" class="add-new-product">New Product</a>
-        </div>
-       </div>
+       <?php 
+        if(!isset($products['message'])) {
+          ?>
+          <div class="heading-and-action">
+            <div>
+              <h1>List of Products</h1>
+            </div>
+            <div>
+              <a href="/product/create" class="add-new-product">New Product</a>
+            </div>
+          </div>
+          <?php
+        }
+       ?>
         <?php if(isset($products['message'])): ?>
           <div class="no-products-found">
             <?php echo $products['message']; ?>
             <div>
-              <a href="/product/create">Add</a>
+              <a href="/product/create" class="empty-add-product-action">Add Product</a>
             </div>
           </div>
         <?php else: ?>
