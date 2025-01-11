cacbed7b5916be4f657a99a79a6d9a7492853ab0
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-01-12 07:22:13 +0800

    Updated routes

--- routes.php
+++ routes.php
@@ -1,4 +1,7 @@
 <?php
 $router->get('/', 'products/index.php');
+$router->get('/product/view', 'products/show.php');
+
 $router->get('/product/create', 'products/create.php');
 $router->post('/product/create', 'products/store.php');
+$router->delete('/product/delete', 'products/destroy.php');
